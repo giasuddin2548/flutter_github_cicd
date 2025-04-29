5438fa072649e09bcf4c87b4df1d0aaeff561069
Gias Uddin Samir <giasuddinsamir@gmail.com>
2025-04-29 17:20:27 +0600

    ios comment out2

--- lib/home.dart
+++ lib/home.dart
@@ -12,7 +12,10 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Github CI-CD")),
-      body: Container(),
+      body: ListView.builder(
+        itemCount: 10,
+        itemBuilder: (c, i) => ListTile(title: Text("Itme Name $i")),
+      ),
     );
   }
 }
